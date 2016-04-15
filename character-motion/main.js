c0930443a702ad29b1c39efba85487a7e3b1843b
Pete Januarius <pete.januarius@gmail.com>
2016-04-15 19:16:23 +1000

    character motion wit

--- character-motion/main.js
+++ character-motion/main.js
@@ -26,8 +26,8 @@ var ship = Crafty.e('2D, Canvas, Color, Solid, Motion, AngularMotion')
                   if(_interval) window.clearInterval(_interval);
                 }
                 /* Work out acceleration vector from angle */
-                ship.acceleration().y = -25
-                ship.acceleration().x = 25 / (Math.tan((90-ship.rotation) * (Math.PI / 180)));
+                ship.acceleration().y = ship.rotation > 90 ? 25 : -25 ;
+                ship.acceleration().x = (ship.rotation > 90 ? -25 : 25) / (Math.tan((90 - ship.rotation) * (Math.PI / 180)));
                 ship.isMoving = true;
               }
               if(e.key === Crafty.keys.LEFT_ARROW){ ship.vrotation = -25; }
@@ -48,17 +48,23 @@ var ship = Crafty.e('2D, Canvas, Color, Solid, Motion, AngularMotion')
             .bind("EnterFrame", function(e){
               if(this.y < -40){ this.y = 601; }
               if(this.y > 602){ this.y = -40; }
+              if(this.x < -40){ this.x = 801; }
+              if(this.x > 840){ this.x = -40; }
             });
 
 function slowToStop(){
   console.log("velocity = "+ship.velocity().y);
   ship.acceleration().y = Math.abs(ship.acceleration().y / 2);
+  ship.acceleration().x = ship.acceleration().y / (Math.tan((90 - ship.rotation) * (Math.PI / 180)));
   ship.velocity().y = ship.velocity().y / _brakeFactor;
+  ship.velocity().x = ship.velocity().x / _brakeFactor;
   if(ship.acceleration().y < 1){
     ship.acceleration().y = 0;
+    ship.acceleration().x = 0;
   }
-  if(ship.velocity().y > -1 && ship.acceleration().y === 0){
+  if(ship.velocity().y > -1 && ship.velocity().y < 1 && ship.acceleration().y === 0){
     ship.velocity().y = 0;
+    ship.velocity().x = 0;
     console.log("Stopping!")
     window.clearInterval(_interval);
     ship.isMoving = false;
